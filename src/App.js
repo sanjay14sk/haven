d62e731dc93b8a48309ecebc81bc764ae0533588
sanjay14sk <sanjay14col@gmail.com>
2026-03-26 23:40:42 +0530

    mini

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [songs, setSongs] = useState([]);
+  const [currentSong, setCurrentSong] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/songs")
+      .then((res) => res.json())
+      .then((data) => setSongs(data));
+  }, []);
+
+  const playSong = (song) => {
+   const audio = new Audio(song.src);
+    audio.play();
+    setCurrentSong(audio);
+  };
+
+  const pauseSong = () => {
+    if (currentSong) {
+      currentSong.pause();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Music App 🎵</h1>
+
+      {songs.map((song) => (
+        <div key={song.id}>
+          <h3>{song.title}</h3>
+          <p>{song.artist}</p>
+          <button onClick={() => playSong(song)}>Play</button>
+        </div>
+      ))}
+      <button onClick={pauseSong}>Pause</button>
     </div>
   );
 }
